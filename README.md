a772f9d2d7c0bf9589bfccda96ca505700fde9bb
khuxkm fbexl <khuxkm@ttm.sh>
2018-10-27 05:13:11 +0000

    Initial code commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sanes
+
+(aka GIMME ALL YOUR DEETA)
+
+A ~~Protogent supporter~~Discord bot that gets server stats and responds to whatever shitty commands you give it.
---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const express = require("express");
+const app = express();
+const fs = require('fs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+app.get(config.http_prefix+"/serverdata.json",function(req,res) {
+	var data = {};
+	const guild = client.guilds.get(502604049825398805);
+	data.name = guild.name;
+	data.count = guild.members.size();
+	res.send(JSON.stringify(data));
+});
+
+client.on("ready", () => {
+	console.log(`*hacker voice* I'm in!`); 
+});
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command=="scream") {}
+
+});
+
+app.listen(config.http_port,function() { console.log("On port "+config.http_port); });
+
+client.login(config.token);
