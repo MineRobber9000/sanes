86d825d6a6755b1340d954cf03369564aac5890c
khuxkm fbexl <khuxkm@ttm.sh>
2018-10-27 07:21:32 +0000

    Add saysomething command

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require("discord.js");
 const express = require("express");
+const request = require("request");
 const app = express();
 const fs = require('fs');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const randItem = function(arr) {
+	return arr[Math.floor(Math.random()*arr.length)];
+};
 
 const client = new Discord.Client();
 
@@ -31,6 +36,22 @@ client.on("message", async message => {
 
 	if (command=="src") {
 		message.channel.send("GitHub: https://github.com/MineRobber9000/sanes");
+	}
+
+	if (command=="saysomething") {
+		var botdata = {responses:["hold up, something's fuccin broken","something's broken #blameminerobber"],usernames:["lemme get that data"],avatars:["https://cdn.discordapp.com/embed/avatars/0.png"]};
+		request("https://khuxkm.tilde.team/sanes/data.json",{json: true},function(err,res,body) {
+			if (err) {console.error(err);} else {
+				botdata = body;
+				console.log(JSON.stringify(botdata));
+			}
+		});
+		const channel = message.channel;
+		channel.createWebhook("temp saysomething webhook","").then(function (wb) {
+			t = new Discord.WebhookClient(wb.id,wb.token);
+			t.send(randItem(botdata.responses),{username:randItem(botdata.usernames),avatar_url:randItem(botdata.avatars)})
+			t.delete("no u");
+		}).catch(console.error);
 	}
 
 });
